aea5d459ece28fd154223052c59ef55630a5f7b6
SouadBEn <souad.bengrina@gmail.com>
2018-10-10 11:43:32 +0200

    souaf

---
+++ src/implementations/Excel.java
@@ -0,0 +1,40 @@
+package implementations;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.io.File;
+import jxl.Workbook;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.Label;
+import jxl.write.WriteException;
+
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class Excel {
+
+    public static final int COL_ID = 0;
+    public static final int COL_NOM = 1;
+    public static final int COL_PRENOM = 2;
+    public static final int COL_STATUT = 3;
+    public static final int COL_PROVENANCE = 4;
+    public static final int COL_FORMATIONPRECEDENTE = 5;
+    public static final int COL_NIVEAUINSERTION = 6;
+    public static final int COL_ID_COURS = 7;
+    public static final int COL_LIBELLE_LCOURS = 8;
+    public static final int COL_TYPE_COURS = 9;
+    public static final int COL_NIVEAU_COURS = 10;
+    public static final int COL_NOTE = 11;
+    private Workbook workbook;
+
+
+
+
+
+
+}
