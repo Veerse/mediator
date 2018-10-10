f73702838b576f6863a92341247afb08fc1aff25
gaperrie <36009919@parisnanterre.fr>
2018-10-10 11:26:17 +0200

    Gaelwork

--- src/contracts/IAdapter.java
+++ src/contracts/IAdapter.java
@@ -3,7 +3,7 @@ package contracts;
 import java.util.ArrayList;
 
 public interface IAdapter {
-
+//ara
     public void connexion();
     public void deconnexion();
     public ArrayList<String> query();
