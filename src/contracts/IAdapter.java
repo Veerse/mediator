ca0311a43822de5a066e11ea8c69728a3b60425a
gaperrie <36009919@parisnanterre.fr>
2018-10-10 11:22:47 +0200

    Gaelwork

--- src/contracts/IAdapter.java
+++ src/contracts/IAdapter.java
@@ -1,5 +1,11 @@
 package contracts;
 
+import java.util.ArrayList;
+
 public interface IAdapter {
+
+    public void connexion();
+    public void deconnexion();
+    public ArrayList<String> query();
     
 }
---
+++ src/implementations/AdapterExcel.java
@@ -0,0 +1,22 @@
+package implementations;
+
+import contracts.IAdapter;
+
+import java.util.ArrayList;
+
+public class AdapterExcel implements IAdapter {
+    @Override
+    public void connexion() {
+
+    }
+
+    @Override
+    public void deconnexion() {
+
+    }
+
+    @Override
+    public ArrayList<String> query() {
+        return null;
+    }
+}
